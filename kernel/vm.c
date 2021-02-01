0ec6155e15777ceb4a16c69ba26fda36a45145e4
Jae <daniel.kwong.h@gmail.com>
2021-01-31 23:13:33 -0800

    tf

--- kernel/vm.c
+++ kernel/vm.c
@@ -298,7 +298,9 @@ uvmdealloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz)
 {
   if(newsz >= oldsz)
     return oldsz;
-  uvmunmap(pagetable, newsz, oldsz - newsz, 1);
+  uint64 newup = PGROUNDUP(newsz);
+  if(newup < PGROUNDUP(oldsz)) 
+    uvmunmap(pagetable, newup, oldsz - newup, 1);
   return newsz;
 }
 
